93462e0801f6f7bba701446dd7c3b9c427b0a524
jetbennett <wolleytej@gmail.com>
2017-04-19 14:47:42 -0400

    Update test_part.cpp
    fixed the brackets that make Christian's head hurt

--- test_part.cpp
+++ test_part.cpp
@@ -15,7 +15,8 @@ using namespace boost;
 const int K = 2;
 idx_t ncon = 1;
 idx_t nparts = K;
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{	
   typedef subgraph< adjacency_list<vecS, vecS, undirectedS, no_property,
     property< edge_index_t, int > > > graph_type;
   typedef graph_traits<graph_type>::vertex_iterator vertex_iter;
@@ -58,11 +59,13 @@ int main(int argc, char* argv[]) {
   xadj[0] = 0;
 
   // Transform the adjacency list into the Compressed Storage Format
-  for(vpair.first; vpair.first != vpair.second; ++vpair.first){
+  for(vpair.first; vpair.first != vpair.second; ++vpair.first)
+  {
     xadj[i+1] = xadj[i] + out_degree(*vpair.first, graph1);
     adjrange_t adjpair = adjacent_vertices(*vpair.first, graph1);
     j = xadj[i];
-    for(adjpair.first; adjpair.first != adjpair.second; ++adjpair.first){
+    for(adjpair.first; adjpair.first != adjpair.second; ++adjpair.first)
+    {
       adjncy[j] = *adjpair.first;
       ++j;
     }
@@ -71,11 +74,13 @@ int main(int argc, char* argv[]) {
 
   // Print out to verify
   cout << "METIS partitioning: " << endl;
-  for(i = 0; i < nvert + 1; ++i){
+  for(i = 0; i < nvert + 1; ++i)
+  {
     cout << xadj[i] << ' ';
   }
   cout << endl;
-  for(i = 0; i < nedge * 2; ++i){
+  for(i = 0; i < nedge * 2; ++i)
+  {
     cout << adjncy[i] << ' ';
   }
   cout << endl;
@@ -85,7 +90,8 @@ int main(int argc, char* argv[]) {
   METIS_PartGraphKway(&nvert, &ncon, xadj, adjncy, NULL, NULL, NULL, &nparts, NULL, NULL, NULL, &objval, part);
 
   // Print out the partition vector
-  for(i=0; i < nvert; ++i){
+  for(i=0; i < nvert; ++i)
+  {
     cout << part[i] << ' ';
   }
   cout << endl;
@@ -93,19 +99,24 @@ int main(int argc, char* argv[]) {
   // Create induced subgraphs based on partition vector
   graph_type *subgraph_vect = new graph_type[K];
   IndexMap index = get(vertex_index, graph1);
-  for (i=0; i<K; ++i) {
+  for (i=0; i<K; ++i) 
+  {
     subgraph_vect[i] = graph1.create_subgraph();
   }
   graph_type::children_iterator ci, ci_end;
-  for (i=0; i < nvert; ++i){
+  for (i=0; i < nvert; ++i)
+  {
     cout << "Mapping " << index[i] << " to subgraph " << part[i] << endl;
     add_vertex(index[i], subgraph_vect[part[i]]);
   }
   /*
   j = 0;
-  for (  boost::tie(ci, ci_end) = graph1.children(); ci != ci_end; ++ci){
-    for (i=0; i < nvert; ++i){
-      if(part[i] == j){
+  for (  boost::tie(ci, ci_end) = graph1.children(); ci != ci_end; ++ci)
+  {
+    for (i=0; i < nvert; ++i)
+    {
+      if(part[i] == j)
+      {
         add_vertex(index[i], *ci);
       }
     }
@@ -122,15 +133,18 @@ int main(int argc, char* argv[]) {
   cout << "root:" << endl;
   print_graph(graph1, get(vertex_index, graph1));
   cout << endl;
-  for (i=0; i < K; ++i){
+  for (i=0; i < K; ++i)
+  {
     cout << "subgraph " << i << ":" << endl;
     cout << "vertices = ";
-    for (boost::tie(v, v_end) = vertices(subgraph_vect[i]); v != v_end; ++v){
+    for (boost::tie(v, v_end) = vertices(subgraph_vect[i]); v != v_end; ++v)
+    {
       cout << subgraph_vect[i].local_to_global(*v) << ", ";
     }
     cout << endl;
     cout << "edges = ";
-    for (boost::tie(e, e_end) = edges(subgraph_vect[i]); e != e_end; ++e){
+    for (boost::tie(e, e_end) = edges(subgraph_vect[i]); e != e_end; ++e)
+    {
       cout << subgraph_vect[i].local_to_global(*e) << ", ";
     }
     cout << endl;
@@ -139,17 +153,20 @@ int main(int argc, char* argv[]) {
   // This does the same thing but will print out the local vertex
   // IDs for each subgraph
 /*
-  for (i=0; i < K; ++i){
+  for (i=0; i < K; ++i)
+  {
     cout << "subgraph " << i << ":" << endl;
     print_graph(subgraph_vect[i], get(vertex_index, subgraph_vect[i]));
   }
   cout << endl;
   int num = 1;
   vertex_iter v, v_end;
-  for (boost::tie(ci, ci_end) = graph1.children(); ci != ci_end; ++ci){
+  for (boost::tie(ci, ci_end) = graph1.children(); ci != ci_end; ++ci)
+  {
     cout << "G" << num++ << ": " << endl;
     cout << "vertices = ";
-    for (boost::tie(v, v_end) = vertices(*ci); v != v_end; ++v){
+    for (boost::tie(v, v_end) = vertices(*ci); v != v_end; ++v)
+    {
       cout << ci->local_to_global(*v) << ", ";
     }
     cout << endl;
@@ -172,14 +189,17 @@ int main(int argc, char* argv[]) {
   // First step is to create the initial row
   cout << "cp3" << endl;
   v_descriptor *avtrow = new v_descriptor[K];
-  for (i=0; i < K; ++i){
+  for (i=0; i < K; ++i)
+  {
     int maxdegree = 0;
     v_descriptor vertID = 0;
     cout << "cp4" << endl;
 
-    for (boost::tie(v, v_end) = vertices(subgraph_vect[i]); v != v_end; ++v){
+    for (boost::tie(v, v_end) = vertices(subgraph_vect[i]); v != v_end; ++v)
+    {
       int temp = out_degree(*v, subgraph_vect[i]);
-      if (temp > maxdegree){
+      if (temp > maxdegree)
+      {
         maxdegree = temp;
 	vertID = subgraph_vect[i].local_to_global(*v);
 	avtrow[i] = vertID;
@@ -199,7 +219,8 @@ int main(int argc, char* argv[]) {
 
   // build ques for BFS
   vert_que *vque_arr = new vert_que[K];
-  for(i=0; i<K; ++i){
+  for(i=0; i<K; ++i)
+  {
     vque_arr[i].push(avtrow[i]);
   }
 
@@ -210,14 +231,17 @@ int main(int argc, char* argv[]) {
   // Print out the AVT
   /*
   cout << endl << "AVT:" << endl;
-  for(i=0; i<1; ++i){
-    for(j=0; j<K; ++j){
+  for(i=0; i<1; ++i)
+  {
+    for(j=0; j<K; ++j)
+    {
       cout << *(avt[i][j]) << ' '; // Segfault ****FML :( *****
     }
     cout << endl;
   }*/
   cout << endl << "AVT:" <<endl;
-  for (i=0; i<K; ++i){
+  for (i=0; i<K; ++i)
+  {
     cout << avtrow[i] << ' ';
   }
 
