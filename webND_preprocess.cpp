baa1f72742dc5fb6543ad4093cba9faa3b165eab
McFlip <gradydenton@yahoo.com>
2017-04-24 13:26:48 -0400

    testing preprocess to cout

--- webND_preprocess.cpp
+++ webND_preprocess.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/vf2_sub_graph_iso.hpp>
 #include <boost/graph/graphviz.hpp>
 using namespace std;
 using namespace boost;
@@ -14,19 +13,13 @@ int main(int argc, char* argv[]) {
  int toNode;
  ifstream inputFile;
  inputFile.open(argv[1]);
+ ofstream outputFile;
+ outputFile.open(argv[2]);
  char trashBuffer[256];
  const int MAX_NODES = 325729;
 
-
- // Build graph1
- int num_vertices1 = 8; graph_type graph1(num_vertices1);
- add_edge(0, 6, graph1); add_edge(0, 7, graph1);
- add_edge(1, 5, graph1); add_edge(1, 7, graph1);
- add_edge(2, 4, graph1); add_edge(2, 5, graph1); add_edge(2, 6, graph1);
- add_edge(3, 4, graph1);
-
- // Build graph2
- graph_type graph2(MAX_NODES);
+// Build graph
+ graph_type graph(MAX_NODES);
 
  inputFile.getline(trashBuffer, 256);
  inputFile.getline(trashBuffer, 256);
@@ -35,27 +28,10 @@ int main(int argc, char* argv[]) {
 
  while(inputFile >> fromNode >> toNode)
  {
-  add_edge(fromNode,  toNode, graph2);
+  add_edge(fromNode,  toNode, graph);
  }
 
- // Create callback to print mappings
- vf2_print_callback<graph_type, graph_type> callback(graph1, graph2);
-
- // Create ordering of small graph
- // Using default instead. Need to fix with example 2 code.
-//  vf2_order_by_mult<graph_type> vos(graph1);
-
- // Print out all subgraph isomorphism mappings between graph1 and graph2.
- // Vertices and edges are assumed to be always equivalent.
-  vf2_subgraph_iso(graph1, graph2, callback);
-
-//   bad attempt at custom predicate function
-//   vf2_subgraph_iso(graph1, graph2, callback, vos, vertices_equivalent([&graph_small, &graph_large](Graph::vertex_descriptor small_vd, Graph::vertex_descriptor large_vd) {
-//     return out_degree(small_vd, graph_small) == out_degree(large_vd, graph_large);})
-//    );
-
-  // print out the graph
-//   write_graphviz(cout, graph2);
+write_graphviz(cout, graph);
 
  return 0;
 }
