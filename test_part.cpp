ac430f1d4097cc06045e2ef6dd1fd23e279d203c
McFlip <gradydenton@yahoo.com>
2017-04-23 20:36:29 -0400

    test

--- test_part.cpp
+++ test_part.cpp
@@ -610,7 +610,7 @@ int main(int argc, char* argv[])
         for(boost::tie(v, v_end) = vertices(subgraph_vect[i]); v != v_end; ++v)
         {
             // if degrees don't match then there is at least one crossing edge
-            cout << "comparing subgraph " << i << ' ' << *v << " to " << ci->local_to_global(*v) << endl;
+            cout << "comparing subgraph " << i << ' ' << *v << " to " << subgraph_vect[i].local_to_global(*v) << endl;
             if (out_degree(*v, *ci) != out_degree(ci->local_to_global(*v), graph1))
             {
                 cout << "Detected a crossing edge from vertex: " << ci->local_to_global(*v) << endl;
